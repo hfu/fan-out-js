eb48614e4a84695a2a13c0101b3f653aa8950dd9
Hidenori Fujimura <hfu@users.noreply.github.com>
2017-12-04 21:18:24 +0000

    update

--- fan-out.js
+++ fan-out.js
@@ -1,5 +1,15 @@
+const fs = require('fs-extra')
+const sqlite3 = require('sqlite3')
+const zlib = require('zlib')
 
 const fan_out = function(mbtiles, dir) {
+  const db = sqlite3.Database(mbtiles, sqlite3.OPEN_READONLY, (err, db) => {
+    if(!err) {
+      console.log(db)
+    } else {
+      console.error(err)
+    }
+  })
 }
 
 if(process.argv.length == 4) {
