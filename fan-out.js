d74df6c235a6692f8899ec1c7cd2882730fbd9d8
Hidenori Fujimura <hfu@users.noreply.github.com>
2017-12-04 22:06:10 +0000

    update

--- fan-out.js
+++ fan-out.js
@@ -1,15 +1,36 @@
 const fs = require('fs-extra')
-const sqlite3 = require('sqlite3')
+const sqlite3 = require('sqlite3').verbose()
 const zlib = require('zlib')
 
 const fan_out = function(mbtiles, dir) {
-  const db = sqlite3.Database(mbtiles, sqlite3.OPEN_READONLY, (err, db) => {
-    if(!err) {
-      console.log(db)
-    } else {
+  const db = new sqlite3.Database(mbtiles, sqlite3.OPEN_READONLY, err => {
+    if(err) {
       console.error(err)
+      return
     }
   })
+  let z, x, y, data, path
+  db.each("SELECT * FROM tiles", (err, r) => {
+    if(err) {
+      console.error(err)
+      return
+    }
+    z = r.zoom_level
+    x = r.tile_column
+    y = (1 << z) - r.tile_row - 1
+    zlib.unzip(r.tile_data, (err, buf) => {
+      if(err) {
+        console.error(err)
+	return
+      }
+      path = `${dir}/${z}/${x}`
+      fs.mkdirsSync(path)
+      path = `${path}/${y}.mvt`
+      fs.writeFileSync(path, buf)
+      console.log(`writing ${path}`)
+    })
+  })
+  db.close
 }
 
 if(process.argv.length == 4) {
