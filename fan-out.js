7725613a91a4ffa1a7996bfc376e40eac2dc3453
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-01-05 14:25:47 -0500

    standard --fix

--- fan-out.js
+++ fan-out.js
@@ -2,16 +2,16 @@ const fs = require('fs-extra')
 const Database = require('better-sqlite3')
 const zlib = require('zlib')
 
-const report = function(c, count, path) {
-  if(c === count || c % 1000 === 0) {
+const report = function (c, count, path) {
+  if (c === count || c % 1000 === 0) {
     console.log(`${c} of ${count} (${Math.round(c * 100.0 / count)}%) ${path}`)
   }
 }
-const run = function(mbtiles, dir) {
+const run = function (mbtiles, dir) {
   const db = new Database(mbtiles, {readonly: true})
   const count = db.prepare('SELECT count(*) FROM tiles').get()['count(*)']
   let c = 0
-  for(const r of db.prepare('SELECT * FROM tiles').iterate()) {
+  for (const r of db.prepare('SELECT * FROM tiles').iterate()) {
     const buf = zlib.unzipSync(r.tile_data)
     const z = r.zoom_level
     const x = r.tile_column
@@ -20,11 +20,11 @@ const run = function(mbtiles, dir) {
     fs.writeFileSync(`${dir}/${z}/${x}/${y}.mvt`, buf)
     report(++c, count, `${dir}/${z}/${x}/${y}.mvt`)
   }
-  db.close
+  db.close()
 }
 
-if(process.argv.length == 4) {
+if (process.argv.length === 4) {
   run(process.argv[2], process.argv[3])
 } else {
-  console.log("usage: node fan-out {tiles.mbtiles} {tiles_dir}")
+  console.log('usage: node fan-out {tiles.mbtiles} {tiles_dir}')
 }
